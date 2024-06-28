40f7a16b1e7d670cad6bbc4b5dfadecdebabb15b
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2024-06-28 22:01:25 +0800

    Added try catch in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,18 @@
 #include <lib/lib.hpp>
 #include <optional>
 
-int main() {
-    std::string filename = "sample.xml";
-    XML_PARSER parser;
-    parser.OpenFile(filename);
-    parser.ScanTags(1);
-    std::cout << "Current Operator: " << parser.GetStackCurrentOperator()[0] << std::endl;
+int main()
+{
+    try
+    {
+        std::string filename = "sample.xml";
+        XML_PARSER parser;
+        parser.OpenFile(filename);
+        parser.ScanTags(1);
+        std::cout << "Current Operator: " << parser.GetStackCurrentOperator()[0] << std::endl;
+    } catch (const char* errout)
+    {
+        std::cout << errout << std::endl;
+    }
     return 0;
 }
